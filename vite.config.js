24221aef34ebcb32a5b01fd0e7aee941988cf3a1
Michnet <ntumichael@gmail.com>
2024-02-26 16:32:12 +0300

    verce

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import vike from 'vike/plugin';
 const path = require('path');
+//import { defineConfig } from "vite"
+//import ssr from "vike/plugin/"
+import vercel from "vite-plugin-vercel"
 
 export default {
   plugins: [vike({
      prerender: true,
      clientRouting: true
-    })],
+    }),
+    //ssr(), 
+    vercel()
+  ],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
